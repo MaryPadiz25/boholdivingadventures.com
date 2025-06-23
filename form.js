213990505cec80e19622c4a7ff0e7fee98141cdc
website <161450854+MaryPadiz25@users.noreply.github.com>
2025-06-23 12:31:29 +0800

    Add files via upload

---
+++ form.js
@@ -0,0 +1,227 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var banner = document.getElementById("banner");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  banner.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+document.addEventListener("DOMContentLoaded", function() {
+  var contacts = document.getElementById("contacts");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  contacts.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var booktour = document.getElementById("booktour");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  booktour.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+
+
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package1 = document.getElementById("package1");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package1.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package2 = document.getElementById("package2");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package2.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package3 = document.getElementById("package3");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package3.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package4 = document.getElementById("package4");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package4.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package5 = document.getElementById("package5");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package5.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package6 = document.getElementById("package6");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package6.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package7 = document.getElementById("package7");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package7.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package8 = document.getElementById("package8");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package8.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package9 = document.getElementById("package9");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package9.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package10 = document.getElementById("package10");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package10.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package11 = document.getElementById("package11");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package11.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package12 = document.getElementById("package12");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package12.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  var package13 = document.getElementById("package13");
+  var divePackage = document.getElementById("divePackage");
+  var closeBanner = document.getElementById("closeBanner");
+
+  setTimeout(function() { divePackage.style.display = "none"; });
+  package13.addEventListener("click", function() {
+    divePackage.style.display = "block";
+  });
+  closeBanner.addEventListener("click", function() {
+    divePackage.style.display = "none";
+});
+});

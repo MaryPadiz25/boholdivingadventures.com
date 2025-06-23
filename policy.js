d2ed006ee63b9d45cf55871b32eef113793f253f
website <161450854+MaryPadiz25@users.noreply.github.com>
2025-06-23 12:41:13 +0800

    Add files via upload

---
+++ policy.js
@@ -0,0 +1,153 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var copyrightArticle = document.getElementById("copyrightArticle");
+  var copyrightPolicy = document.getElementById("copyrightPolicy");
+   var closeCopyright = document.getElementById("closeCopyright");
+ 
+   setTimeout(function() { copyrightArticle.style.display = "none"; });
+    copyrightPolicy.addEventListener("click", function() {
+        copyrightArticle.style.display = "block";
+    });
+   
+    closeCopyright.addEventListener("click", function() {
+        copyrightArticle.style.display = "none";
+    });
+   
+  });
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var privacyArticle2 = document.getElementById("privacyArticle2");
+  var privacyPolicy = document.getElementById("privacyPolicy");
+   var closeArticle = document.getElementById("closeArticle");
+  
+    setTimeout(function() { privacyArticle2.style.display = "none"; });
+    privacyPolicy.addEventListener("click", function() {
+        privacyArticle2.style.display = "block";
+    });
+   
+    closeArticle.addEventListener("click", function() {
+        privacyArticle2.style.display = "none";
+    });
+   
+  });
+
+    document.addEventListener("DOMContentLoaded", function() {
+    var privacyArticle2 = document.getElementById("privacyArticle2");
+  var privacyPolicys1 = document.getElementById("privacyPolicys1");
+   var closeArticle = document.getElementById("closeArticle");
+  
+    setTimeout(function() { privacyArticle2.style.display = "none"; });
+    privacyPolicys1.addEventListener("click", function() {
+        privacyArticle2.style.display = "block";
+    });
+   
+    closeArticle.addEventListener("click", function() {
+        privacyArticle2.style.display = "none";
+    });
+   
+  });
+
+      document.addEventListener("DOMContentLoaded", function() {
+    var privacyArticle2 = document.getElementById("privacyArticle2");
+  var privacyPolicys2 = document.getElementById("privacyPolicys2");
+   var closeArticle = document.getElementById("closeArticle");
+  
+    setTimeout(function() { privacyArticle2.style.display = "none"; });
+    privacyPolicys2.addEventListener("click", function() {
+        privacyArticle2.style.display = "block";
+    });
+   
+    closeArticle.addEventListener("click", function() {
+        privacyArticle2.style.display = "none";
+    });
+   
+  });
+     document.addEventListener("DOMContentLoaded", function() {
+    var privacyArticle2 = document.getElementById("privacyArticle2");
+  var privacyPolicys3 = document.getElementById("privacyPolicys3");
+   var closeArticle = document.getElementById("closeArticle");
+  
+    setTimeout(function() { privacyArticle2.style.display = "none"; });
+    privacyPolicys3.addEventListener("click", function() {
+        privacyArticle2.style.display = "block";
+    });
+   
+    closeArticle.addEventListener("click", function() {
+        privacyArticle2.style.display = "none";
+    });
+   
+  });
+     document.addEventListener("DOMContentLoaded", function() {
+    var privacyArticle2 = document.getElementById("privacyArticle2");
+  var privacyPolicys4 = document.getElementById("privacyPolicys4");
+   var closeArticle = document.getElementById("closeArticle");
+  
+    setTimeout(function() { privacyArticle2.style.display = "none"; });
+    privacyPolicys4.addEventListener("click", function() {
+        privacyArticle2.style.display = "block";
+    });
+   
+    closeArticle.addEventListener("click", function() {
+        privacyArticle2.style.display = "none";
+    });
+   
+  });
+     document.addEventListener("DOMContentLoaded", function() {
+    var privacyArticle2 = document.getElementById("privacyArticle2");
+  var privacyPolicys5 = document.getElementById("privacyPolicys5");
+   var closeArticle = document.getElementById("closeArticle");
+  
+    setTimeout(function() { privacyArticle2.style.display = "none"; });
+    privacyPolicys5.addEventListener("click", function() {
+        privacyArticle2.style.display = "block";
+    });
+   
+    closeArticle.addEventListener("click", function() {
+        privacyArticle2.style.display = "none";
+    });
+   
+  });
+     document.addEventListener("DOMContentLoaded", function() {
+    var privacyArticle2 = document.getElementById("privacyArticle2");
+  var privacyPolicys6 = document.getElementById("privacyPolicys6");
+   var closeArticle = document.getElementById("closeArticle");
+  
+    setTimeout(function() { privacyArticle2.style.display = "none"; });
+    privacyPolicys6.addEventListener("click", function() {
+        privacyArticle2.style.display = "block";
+    });
+   
+    closeArticle.addEventListener("click", function() {
+        privacyArticle2.style.display = "none";
+    });
+   
+  });
+     document.addEventListener("DOMContentLoaded", function() {
+    var privacyArticle2 = document.getElementById("privacyArticle2");
+  var privacyPolicys7 = document.getElementById("privacyPolicys7");
+   var closeArticle = document.getElementById("closeArticle");
+  
+    setTimeout(function() { privacyArticle2.style.display = "none"; });
+    privacyPolicys2.addEventListener("click", function() {
+        privacyArticle7.style.display = "block";
+    });
+   
+    closeArticle.addEventListener("click", function() {
+        privacyArticle2.style.display = "none";
+    });
+   
+  });
+     document.addEventListener("DOMContentLoaded", function() {
+    var privacyArticle2 = document.getElementById("privacyArticle2");
+  var privacyPolicys8 = document.getElementById("privacyPolicys8");
+   var closeArticle = document.getElementById("closeArticle");
+  
+    setTimeout(function() { privacyArticle2.style.display = "none"; });
+    privacyPolicys2.addEventListener("click", function() {
+        privacyArticle8.style.display = "block";
+    });
+   
+    closeArticle.addEventListener("click", function() {
+        privacyArticle2.style.display = "none";
+    });
+   
+  });
